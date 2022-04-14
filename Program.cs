435c69838ebca02bf75488e873c9cfc2c74e7e20
Troksi10 <98901818+Troksi10@users.noreply.github.com>
2022-04-14 18:00:06 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,37 +1,30 @@
-﻿using System;
+﻿// See https://aka.ms/new-console-template for more information
 
-namespace Real_Calculator
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
 
-            // Create new console application “RealCalculator” that takes two numbers from the input and asks what operation would the user want to be done ( +, - , * , / ). Then it returns the result.
-            //Test Data:
-            //Enter the First number: 10
-            //Enter the Second number: 15
-            //Enter the Operation: +
-            //Expected Output:
-            //The result is: 25
+//Create new console application “AverageNumber” that takes four numbers as input to calculate and print the average.
 
-            Console.WriteLine("Enter the First Number");
-            int num1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter the Second Number");
-            int num2 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter the operation:");
-            string operation = Console.ReadLine();
+//Test Data:
+//Enter the First number: 10
+//Enter the Second number: 15
+//Enter the third number: 20
+//Enter the four number: 30
+//Expected Output:
+//The average of 10, 15, 20 and 30 is: 18
 
-            if (operation == "+")
-            {
-                Console.WriteLine(num1 + num2);
-            }
-            else
-            {
-                Console.WriteLine("Invalid operation");
-            }
-            
-            Console.ReadLine();
-        }
-    }
-}
+Console.WriteLine("Enter the First Number");
+int firstNum = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter the Second Number");
+int secondNum = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter the Third Number");
+int thirdNum = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter the Fourth Number");
+int fourthNum = Convert.ToInt32(Console.ReadLine());
+
+int AverageNumber = (firstNum + secondNum + thirdNum + fourthNum) / 4;
+Console.WriteLine($"The average of 10,15,20 and 30 is : {AverageNumber}");
+
+
+Console.ReadLine();
+
+
+

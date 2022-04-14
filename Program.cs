b874c10b85fc00d37dff0e52e4089f06be07a71c
Troksi10 <98901818+Troksi10@users.noreply.github.com>
2022-04-14 17:33:23 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Real_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Create new console application “RealCalculator” that takes two numbers from the input and asks what operation would the user want to be done ( +, - , * , / ). Then it returns the result.
+            //Test Data:
+            //Enter the First number: 10
+            //Enter the Second number: 15
+            //Enter the Operation: +
+            //Expected Output:
+            //The result is: 25
+
+            Console.WriteLine("Enter the First Number");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Second Number");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the operation:");
+            string operation = Console.ReadLine();
+
+            if (operation == "+")
+            {
+                Console.WriteLine(num1 + num2);
+            }
+            else
+            {
+                Console.WriteLine("Invalid operation");
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
